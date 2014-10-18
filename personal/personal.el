a7795b6855fe9615220a7ecf705a02e739e3e8b5
Patrick Rodriguez <patr84@gmail.com>
2014-10-18 10:05:45 -0700

    added keyboard shortcut to edit personal.el file

--- personal/personal.el
+++ personal/personal.el
@@ -2,3 +2,9 @@
 (setq mac-option-modifier 'super)
 
 (setq ring-bell-function 'ignore)
+
+(defun edit-personal ()
+  (interactive)
+  (find-file "~/.emacs.d/personal/personal.el"))
+
+(global-set-key (kbd "C-x M-p") 'edit-personal)
